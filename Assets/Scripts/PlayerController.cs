84325df74b9928c1080a629063e8ca5e4be6ab71
Matheus Rocco <matheus.rocco@eemovel.com>
2021-11-29 07:57:31 -0300

    feat: call end video on checkpoint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,7 +12,15 @@ public class PlayerController : MonoBehaviour
     private float verticalInput;
     private float horizontalInput;
 
-    // Update is called once per frame
+    public GameObject policeCar;
+    Vector3 spawnPoint;
+
+    void Start() 
+    {
+        spawnPoint = gameObject.transform.position;
+    }
+
+
     void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
@@ -20,5 +29,23 @@ public class PlayerController : MonoBehaviour
 
         if (Mathf.Abs(verticalInput) > 0)
             transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+
+        if (gameObject.transform.position.y < -20f)
+        {
+            gameObject.transform.position = spawnPoint;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (other.gameObject.CompareTag("police"))
+        {
+            gameObject.transform.position = spawnPoint;
+        }
     }
 }
